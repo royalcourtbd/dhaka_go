e49f18999390a7af306ad4599b5674c55a288523
royalcourtbd <sayedvai08@gmail.com>
2025-11-19 22:45:00 +0600

    Add Firebase and related plugins, update dependencies
    Added firebase_core, cloud_firestore, firebase_crashlytics, firebase_analytics, firebase_messaging, lottie, responsive_sizer, flutter_native_splash, and change_app_package_name to pubspec.yaml and updated pubspec.lock. Registered new plugins for macOS and Windows. Removed the default widget test file.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import device_info_plus
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
+import firebase_messaging
 import package_info_plus
 import path_provider_foundation
 import share_plus
@@ -13,7 +18,12 @@ import sqlite3_flutter_libs
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  cloud_firestore
+  firebase_core
   share_plus
   sqlite3_flutter_libs
   url_launcher_windows
